b52599c315bb2044513d3818216db7c50b66a7c3
Amelie Risi <amerisi@emi.u-bordeaux1.fr>
2016-12-01 00:11:43 +0100

    modification en cours du script pour all_edges.csv

--- panamas/neo4j/import/script_all_edges.py
+++ panamas/neo4j/import/script_all_edges.py
@@ -2,5 +2,22 @@
 
 import csv
 
-with open('all_edges.csv', 'rb') as csvfile:
-    reader = csv.reader(csvfile)
+edges = open('all_edges.csv', 'rb')
+edgescsv = csv.reader(edges, delimiter=',')
+liste = []
+for row in edgescsv:
+    row = [col.lower() for col in row]
+    row = [col.strip() for col in row]
+    if not row[1] in liste:
+        liste.append(row[1])
+
+liste
+edges.close()
+
+
+with open('all_edges.csv', 'rb') as edges_file:
+    reader = csv.DictReader(edges_file)
+    data = {}
+    for row in reader:
+        for header , values in row.items():
+            
