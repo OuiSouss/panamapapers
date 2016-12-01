3c6df3950a461b5b32579e62979690dfcb8ede65
Amelie Risi <amerisi@emi.u-bordeaux1.fr>
2016-12-01 22:59:39 +0100

    modification dans script pour aretes avec liste et tri

--- panamas/neo4j/import/script_all_edges.py
+++ panamas/neo4j/import/script_all_edges.py
@@ -7,17 +7,13 @@ edgescsv = csv.reader(edges, delimiter=',')
 liste = []
 for row in edgescsv:
     row = [col.lower() for col in row]
-    row = [col.strip() for col in row]
     if not row[1] in liste:
         liste.append(row[1])
-
-liste
+        
+liste.pop(0)
+liste.sort()
+print (liste)
+print (len(liste))
 edges.close()
 
-
-with open('all_edges.csv', 'rb') as edges_file:
-    reader = csv.DictReader(edges_file)
-    data = {}
-    for row in reader:
-        for header , values in row.items():
             
