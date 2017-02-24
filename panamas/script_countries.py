6ab4e937d39a84ae4365e8e33f490a90a0020d98
Amelie Risi <amerisi@emi.u-bordeaux1.fr>
2017-02-24 01:06:30 +0100

    script pour creer noeud des pays complete et fonctionnel

--- panamas/script_countries.py
+++ panamas/script_countries.py
@@ -1,20 +1,38 @@
-from neo4j.v1 import GraphDatabase, basic_auth
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-s
 
+from neo4j.v1 import GraphDatabase, basic_auth
+#import os
 
 driver = GraphDatabase.driver("bolt://localhost", auth=basic_auth("neo4j", "neo"))
 session = driver.session()
 
 result = session.run("MATCH (n) WHERE EXISTS(n.countries) RETURN DISTINCT \"node\" as element, n.countries AS countries")
 
-countries = []
+l_countries = []
 for r in result:
-    c = r["countries"]
-    c = c.split()
-    for i in c:
-        if i not in countries:
-            countries.append(i)
-    print (countries)
-    print ("%s "%r["countries"])
+    i = r["countries"]
+    i = i.encode("utf-8")
+    c = i.split(";")
+    for s in c :
+        if s not in l_countries:
+            l_countries.append(s)
 
+print (len(l_countries))
+#print (l_countries[106])
+#print (l_countries[107])
+#print (l_countries[108])
+
+
+#os.system("{ echo \"export pays='France'\" ; cat; } | neo4j/bin/neo4j-shell")
+
+for n in l_countries:
+    n = n.decode('utf-8')
+    n = n.replace("'", " ")
+    c = "'%s'"%n
+    print (c)
+    session.run("create (c:Country{country:%s})"%c)
+
+    
 session.close()
 
